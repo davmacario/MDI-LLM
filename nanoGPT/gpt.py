ea95f991c212339164fd9118203517fd525b8bce
davmacario <dav.macario@gmail.com>
2024-01-22 11:59:32 -0600

    [feat]: implemented basic bigram model with context size 1

--- nanoGPT/gpt.py
+++ nanoGPT/gpt.py
@@ -9,7 +9,8 @@ from torch.nn import functional as F
 VERB = True
 CURR_DIR = os.path.dirname(__file__)
 BLOCK_SIZE = 8  # (context length)
-BATCH_SIZE = 4
+BATCH_SIZE = 32
+N_ITER_TRAIN = 50000
 
 
 class BigramLanguageModel(nn.Module):
@@ -20,12 +21,60 @@ class BigramLanguageModel(nn.Module):
         # Embedding table of size vocab_size x vocab_size
         self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
 
-    def forward(self, idx, targets):
-        """Forward pass in bigram embedding"""
+    def forward(self, idx: torch.Tensor, targets: torch.Tensor | None = None):
+        """
+        Forward pass in bigram embedding.
+
+        Args:
+            idx: (Batch size) x (Time) tensor of integers
+            targets: target embedding, same size as idx
+
+        Returns:
+            logits - row 'idx' of the token embedding table; size is BxTxC
+        """
+
         # The logits returned are the ones in row idx of the table
         # This is arranged in a tensor of size Batch x Time x Channel
         logits = self.token_embedding_table(idx)
-        return logits
+
+        if targets is not None:
+            # Conform to PyTorch's specs
+            B, T, C = logits.shape
+            logits = logits.view(B * T, C)
+
+            targets = targets.view(B * T)
+            loss = F.cross_entropy(logits, targets)
+        else:
+            loss = None
+
+        return logits, loss
+
+    def generate(self, idx: torch.Tensor, max_new_tokens: int):
+        """
+        Generate new tokens using the Bigram Language Model, provided the input
+        sequence of integers "idx".
+
+        Args:
+            idx: input sequence of encoded chars/words (B x T)
+            max_new_tokens: maximum number of tokens to be generated
+
+        Returns:
+            Updated version of idx, containing the generated elements
+        """
+        for _ in range(max_new_tokens):
+            # Get predictions
+            logits, _ = self(idx)
+            # Focus only on last time step (dim: (B, C))
+            # Now, the model chooses the next token based on the last one only
+            logits = logits[:, -1, :]  # B x C
+            # Apply softmax to get probabilities of tokens in the last time step
+            probs = F.softmax(logits, dim=1)
+            # Sample p.m.f
+            idx_next = torch.multinomial(probs, num_samples=1)  # B x 1
+            # Append sampled index to idx
+            idx = torch.cat((idx, idx_next), dim=1)  # B x (T+1)
+
+        return idx
 
 
 def main():
@@ -73,7 +122,7 @@ def main():
             target = y[t]
             print(f"When the input is {context}, the target is {target}")
 
-    # Training
+    # Dataloader
     torch.manual_seed(1337)
 
     def get_batch(split: str):
@@ -90,7 +139,7 @@ def main():
         data = train_data if split == "train" else val_data
         ix = torch.randint(len(data) - BLOCK_SIZE, (BATCH_SIZE,))
         x = torch.stack([data[i : i + BLOCK_SIZE] for i in ix])
-        y = torch.stack([data[i * 1 : i * BLOCK_SIZE + 1] for i in ix])
+        y = torch.stack([data[i + 1 : i + BLOCK_SIZE + 1] for i in ix])
         return x, y
 
     xb, yb = get_batch("train")
@@ -114,9 +163,46 @@ def main():
 
     torch.manual_seed(1337)
     m = BigramLanguageModel(vocab_size)
-    out = m(xb, yb)
+    out, loss = m(xb, yb)
     if VERB:
         print(out.shape)
+        print(f"Loss: {loss}")
+
+    # --------------- Generate without training: ----------------------
+    # Kick off the generation (0: new line)
+    idx = torch.zeros((1, 1), dtype=torch.long)
+    # Index to [0] because the 1st dim is batches (we have 1 batch)
+    gen_text = decode(m.generate(idx, max_new_tokens=100)[0].tolist())
+    if VERB:
+        print(gen_text)
+
+    # --------------- Training the Bigram model -----------------------
+    # Create PyTorch optimizer (AdamW)
+    optimizer = torch.optim.AdamW(m.parameters(), lr=1e-3)
+
+    if VERB:
+        print("Started training:")
+
+    for steps in range(N_ITER_TRAIN):
+        # Typical training loop
+
+        # Sample batch of data
+        xb, yb = get_batch("train")
+
+        # Evaluate loss
+        logits, loss = m(xb, yb)
+        optimizer.zero_grad(set_to_none=True)
+        loss.backward()
+        optimizer.step()
+
+    if VERB:
+        print(f"Loss: {loss.item()}")
+
+    idx = torch.zeros((1, 1), dtype=torch.long)
+    gen_text = decode(m.generate(idx, max_new_tokens=100)[0].tolist())
+    if VERB:
+        print("After training:")
+        print(gen_text)
 
 
 if __name__ == "__main__":
