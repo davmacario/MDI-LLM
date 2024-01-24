ec8d0f243219be84ac7f85bca36dab38ba089585
davmacario <dav.macario@gmail.com>
2024-01-23 21:22:56 -0600

    [fix]: improve imports

--- nanoGPT/sub/__init__.py
+++ nanoGPT/sub/__init__.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 
-from sub.config import (BATCH_SIZE, BLOCK_SIZE, DEVICE, DROPOUT, EVAL_INTERVAL,
-                        EVAL_ITERS, LEARNING_RATE, N_EMBD, N_HEADS,
-                        N_ITER_TRAIN, N_LAYER)
+from . import model
+from .char_tokenizer import CharacterTokenizer
+from .config import (BATCH_SIZE, BLOCK_SIZE, DEVICE, DROPOUT, EVAL_INTERVAL,
+                     EVAL_ITERS, LEARNING_RATE, N_EMBD, N_HEADS, N_ITER_TRAIN,
+                     N_LAYER)
+from .data_loader import load_dataset, split_dataset
+from .model import GPT
 
 __all__ = [
     "BATCH_SIZE",
@@ -16,4 +20,9 @@ __all__ = [
     "N_HEADS",
     "N_ITER_TRAIN",
     "N_LAYER",
+    "CharacterTokenizer",
+    "GPT",
+    "load_dataset",
+    "split_dataset",
+    "model",
 ]
