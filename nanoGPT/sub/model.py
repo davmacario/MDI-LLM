c306da05c3dedd95d22083640dff02fac677f3cb
davmacario <dav.macario@gmail.com>
2024-01-25 16:02:25 -0600

    [feat]: add methods to the GPT class

--- nanoGPT/sub/model.py
+++ nanoGPT/sub/model.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import inspect
 import os
 from dataclasses import dataclass
 
@@ -7,9 +8,9 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
-from sub.config import (BATCH_SIZE, BLOCK_SIZE, DEVICE, DROPOUT, EVAL_INTERVAL,
-                        EVAL_ITERS, LEARNING_RATE, N_EMBD, N_HEADS,
-                        N_ITER_TRAIN, N_LAYER)
+from sub.config import (BATCH_SIZE, BIAS, BLOCK_SIZE, DEVICE, DROPOUT,
+                        EVAL_INTERVAL, EVAL_ITERS, LEARNING_RATE, N_EMBD,
+                        N_HEADS, N_ITER_TRAIN, N_LAYER)
 
 
 class Head(nn.Module):
@@ -149,12 +150,14 @@ class GPTConfig:
 
     batch_size: int = BATCH_SIZE  # FIXME: it wasn't here before, see if needed
     block_size: int = BLOCK_SIZE  # Context length
-    vocab_size: int = 50304  # from GPT-2: 50257 (round to multiple of 64)
+    vocab_size: int | None = (
+        50304  # from GPT-2: 50257 (round to multiple of 64)
+    )
     n_layer: int = N_LAYER  # Number of transformer blocks
     n_head: int = N_HEADS
     n_embd: int = N_EMBD
     dropout: float = DROPOUT
-    bias: bool = True  # True: bias in Linears and LayerNorms, like GPT-2. False: a bit better and faster
+    bias: bool = BIAS  # True: bias in Linears and LayerNorms, like GPT-2. False: a bit better and faster
     device: str = DEVICE  # FIXME: new - where to put this?
 
 
@@ -242,6 +245,66 @@ class GPT(nn.Module):
         if non_embedding:
             n_params -= self.transformer.position_embedding.weight.numel()
         return n_params
+
+    def configure_optimizers(
+        self, weight_decay, learning_rate, betas, device_type
+    ):
+        """
+        TODO
+        """
+        # start with all of the candidate parameters
+        param_dict = {pn: p for pn, p in self.named_parameters()}
+        # filter out those that do not require grad
+        param_dict = {pn: p for pn, p in param_dict.items() if p.requires_grad}
+        # create optim groups. Any parameters that is 2D will be weight decayed, otherwise no.
+        # i.e. all weight tensors in matmuls + embeddings decay, all biases and layernorms don't.
+        decay_params = [p for _, p in param_dict.items() if p.dim() >= 2]
+        nodecay_params = [p for _, p in param_dict.items() if p.dim() < 2]
+        optim_groups = [
+            {"params": decay_params, "weight_decay": weight_decay},
+            {"params": nodecay_params, "weight_decay": 0.0},
+        ]
+        num_decay_params = sum(p.numel() for p in decay_params)
+        num_nodecay_params = sum(p.numel() for p in nodecay_params)
+        print(
+            f"num decayed parameter tensors: {len(decay_params)}, with {num_decay_params:,} parameters"
+        )
+        print(
+            f"num non-decayed parameter tensors: {len(nodecay_params)}, with {num_nodecay_params:,} parameters"
+        )
+        # Create AdamW optimizer and use the fused version if it is available
+        fused_available = (
+            "fused" in inspect.signature(torch.optim.AdamW).parameters
+        )
+        use_fused = fused_available and device_type == "cuda"
+        extra_args = dict(fused=True) if use_fused else dict()
+        optimizer = torch.optim.AdamW(
+            optim_groups, lr=learning_rate, betas=betas, **extra_args
+        )
+        print(f"using fused AdamW: {use_fused}")
+
+        return optimizer
+
+    def estimate_mfu(self, fwdbwd_per_iter, dt):
+        """estimate model flops utilization (MFU) in units of A100 bfloat16 peak FLOPS"""
+        # first estimate the number of flops we do per iteration.
+        # see PaLM paper Appendix B as ref: https://arxiv.org/abs/2204.02311
+        N = self.get_num_params()
+        cfg = self.config
+        L, H, Q, T = (
+            cfg.n_layer,
+            cfg.n_head,
+            cfg.n_embd // cfg.n_head,
+            cfg.block_size,
+        )
+        flops_per_token = 6 * N + 12 * L * H * Q * T
+        flops_per_fwdbwd = flops_per_token * T
+        flops_per_iter = flops_per_fwdbwd * fwdbwd_per_iter
+        # express our flops throughput as ratio of A100 bfloat16 peak flops
+        flops_achieved = flops_per_iter * (1.0 / dt)  # per second
+        flops_promised = 312e12  # A100 GPU bfloat16 peak flops is 312 TFLOPS
+        mfu = flops_achieved / flops_promised
+        return mfu
 
     def forward(self, idx: torch.Tensor, targets: torch.Tensor | None = None):
         """
