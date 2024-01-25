ec63a7067f1649bcae21fca13406e706740acb41
davmacario <dav.macario@gmail.com>
2024-01-25 11:43:44 -0600

    [fix]: review imports

--- nanoGPT/sub/__init__.py
+++ nanoGPT/sub/__init__.py
@@ -1,26 +1,11 @@
 #!/usr/bin/env python3
 
-from . import data_loader, model
+from . import config, data_loader, model
 from .char_tokenizer import CharacterTokenizer
-from .config import (BATCH_SIZE, BLOCK_SIZE, DEVICE, DROPOUT, EVAL_INTERVAL,
-                     EVAL_ITERS, LEARNING_RATE, N_EMBD, N_HEADS, N_ITER_TRAIN,
-                     N_LAYER)
-from .model import GPT
 
 __all__ = [
-    "BATCH_SIZE",
-    "BLOCK_SIZE",
-    "DEVICE",
-    "DROPOUT",
-    "EVAL_INTERVAL",
-    "EVAL_ITERS",
-    "LEARNING_RATE",
-    "N_EMBD",
-    "N_HEADS",
-    "N_ITER_TRAIN",
-    "N_LAYER",
+    "config",
     "CharacterTokenizer",
-    "GPT",
     "data_loader",
     "model",
 ]
