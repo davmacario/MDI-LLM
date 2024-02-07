3016689e0fbfecf24729b8a193ef8a7199ba12d9
davmacario <dav.macario@gmail.com>
2024-02-07 10:03:04 -0600

    [fix]: tidy code, remove unused stuff

--- nanoGPT/finisher.py
+++ nanoGPT/finisher.py
@@ -1,57 +1,30 @@
 #!/usr/bin/env python3
 
-import inspect
 import json
 import os
-import time
-import warnings
-from contextlib import nullcontext
-from dataclasses import dataclass
-from typing import Any, Dict, List, Mapping, Union
 
 import cherrypy as cp
-import requests
 import torch
-import torch.nn as nn
-from torch.nn import functional as F
 
-from sub.config import (BATCH_SIZE, BIAS, BLOCK_SIZE, CKPT_INTERVAL, DEVICE,
-                        DROPOUT, DTYPE, EVAL_ITERS, LEARNING_RATE, N_EMBD,
-                        N_HEADS, N_ITER_TRAIN, N_LAYER, VERB)
-from sub.model import GPTConfig
-from sub.model_dist import GPTDistributed, GPTServer
+from sub.model_dist import GPTServer
 
 # -----------------------------------------------------------------------------
-script_dir = os.path.dirname(__file__)
-settings_path = os.path.join(script_dir, "settings_distr")
-network_conf_path = os.path.join(settings_path, "configuration.json")
+if __name__ == "__main__":
+    torch.manual_seed(1337)
+    torch.cuda.manual_seed(1337)
+    torch.backends.cuda.matmul.allow_tf32 = True  # allow tf32 on matmul
+    torch.backends.cudnn.allow_tf32 = True  # allow tf32 on cudnn
 
-torch.manual_seed(1337)
-torch.cuda.manual_seed(1337)
-torch.backends.cuda.matmul.allow_tf32 = True  # allow tf32 on matmul
-torch.backends.cudnn.allow_tf32 = True  # allow tf32 on cudnn
-# for later use in torch.autocast:
-if "cuda" in DEVICE:
-    device_type = "cuda"
-elif "mps" in DEVICE:
-    device_type = "mps"
-else:
-    device_type = "cpu"
-ptdtype = {
-    "float32": torch.float32,
-    "bfloat16": torch.bfloat16,
-    "float16": torch.float16,
-}[DTYPE]
-ctx = (
-    nullcontext()
-    if device_type in {"cpu", "mps"}
-    else torch.amp.autocast(device_type=device_type, dtype=ptdtype)
-)
+    script_dir = os.path.dirname(__file__)
+    settings_path = os.path.join(script_dir, "settings_distr")
+    network_conf_path = os.path.join(settings_path, "configuration.json")
 
-try:
-    with open(network_conf_path, "r") as f:
-        full_config = json.load(f)
-        gpt_webserv = GPTServer(node_config=full_config["nodes"]["finisher"])
-except KeyboardInterrupt:
-    print("Node stopped!")
-    cp.engine.stop()
+    try:
+        with open(network_conf_path, "r") as f:
+            full_config = json.load(f)
+            gpt_webserv = GPTServer(
+                node_config=full_config["nodes"]["finisher"]
+            )
+    except KeyboardInterrupt:
+        print("Node stopped!")
+        cp.engine.stop()
--- nanoGPT/starter.py
+++ nanoGPT/starter.py
@@ -1,25 +1,11 @@
 #!/usr/bin/env python3
 
-import inspect
-import json
 import os
-import time
-import warnings
-from contextlib import nullcontext
-from dataclasses import dataclass
-from typing import Any, Dict, List, Mapping, Union
 
 import cherrypy as cp
-import requests
 import torch
-import torch.nn as nn
-from torch.nn import functional as F
 
-from sub.config import (BATCH_SIZE, BIAS, BLOCK_SIZE, CKPT_INTERVAL, DEVICE,
-                        DROPOUT, DTYPE, EVAL_ITERS, LEARNING_RATE, N_EMBD,
-                        N_HEADS, N_ITER_TRAIN, N_LAYER, VERB)
-from sub.model import GPTConfig
-from sub.model_dist import GPTDistributed, GPTServer
+from sub.model_dist import GPTDistributed
 
 # -----------------------------------------------------------------------------
 script_dir = os.path.dirname(__file__)
@@ -29,24 +15,6 @@ torch.manual_seed(1337)
 torch.cuda.manual_seed(1337)
 torch.backends.cuda.matmul.allow_tf32 = True  # allow tf32 on matmul
 torch.backends.cudnn.allow_tf32 = True  # allow tf32 on cudnn
-# for later use in torch.autocast:
-if "cuda" in DEVICE:
-    device_type = "cuda"
-elif "mps" in DEVICE:
-    device_type = "mps"
-else:
-    device_type = "cpu"
-ptdtype = {
-    "float32": torch.float32,
-    "bfloat16": torch.bfloat16,
-    "float16": torch.float16,
-}[DTYPE]
-ctx = (
-    nullcontext()
-    if device_type in {"cpu", "mps"}
-    else torch.amp.autocast(device_type=device_type, dtype=ptdtype)
-)
-
 
 if __name__ == "__main__":
     dataset_name = os.path.splitext(dataset)[0]
