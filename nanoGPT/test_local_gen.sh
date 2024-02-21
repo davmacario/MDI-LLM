107b613fd4ab9c00f43943a0b8ffdfd430869db4
davmacario <dav.macario@gmail.com>
2024-02-21 10:51:17 -0600

    [fix]: add error message for missing args

--- nanoGPT/test_local_gen.sh
+++ nanoGPT/test_local_gen.sh
@@ -4,7 +4,13 @@
 # Perform 10 runs with the same model - the "name" is the 1st command line arg
 # Example usage:
 #       ./test_local_gen.sh 7layers
-for i in $(seq 1 10);
-do
-    python3 "$(dirname $0)"/sample.py --ckpt="$(dirname $0)"/data/shakespeare/out/ckpt_"$1".pt --time-run="$(dirname $0)"/logs/run_times_single_"$1".csv --verb
-done
+
+if [ "$#" -eq 0 ];
+then
+    echo "Missing model type/name in args"
+else
+    for i in $(seq 1 10);
+    do
+        python3 "$(dirname $0)"/sample.py --ckpt="$(dirname $0)"/data/shakespeare/out/ckpt_"$1".pt --time-run="$(dirname $0)"/logs/run_times_single_"$1".csv --verb
+    done
+fi
