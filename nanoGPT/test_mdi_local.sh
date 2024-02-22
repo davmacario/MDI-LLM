094646e97312017a8d1324d322556c7498351829
davmacario <dav.macario@gmail.com>
2024-02-22 10:33:47 -0600

    [fix]: improve script to test mdi

--- nanoGPT/test_mdi_local.sh
+++ nanoGPT/test_mdi_local.sh
@@ -8,14 +8,21 @@
 if [ "$#" -eq 0 ];
 then
     echo "ERROR: missing model type/name in args"
-    exit
+    exit 1
 fi
+
+if [ ! -f "$(dirname $0)"/data/shakespeare/out/ckpt_"$1".pt ];
+then
+    echo "ERROR: $(dirname ${0})/data/shakespeare/out/ckpt_${1}.pt} does not exist"
+    exit 2
+fi
+
 for i in $(seq 1 10);
 do
     echo "Launching run number ${i}"
-    python3 "$(dirname $0)"/finisher.py &
-    python3 "$(dirname $0)"/intermediate.py &
-    python3 "$(dirname $0)"/starter.py \
-        --ckpt="$(dirname $0)"/data/shakespeare/out/ckpt_"$1".pt \
-        --time-run="$(dirname $0)"/logs/run_times_mdi_single_"$1".csv
+    python3 "$(dirname ${0})"/finisher.py &
+    python3 "$(dirname ${0})"/intermediate.py &
+    python3 "$(dirname ${0})"/starter.py \
+        --ckpt="$(dirname ${0})"/data/shakespeare/out/ckpt_"${1}".pt \
+        --time-run="$(dirname ${0})"/logs/run_times_mdi_single_"${1}".csv
 done
