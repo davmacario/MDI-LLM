0e44766d31dca2323b62bfdd87c47504fe0cef43
davmacario <dav.macario@gmail.com>
2024-02-22 13:53:25 -0600

    [feat]: improve test scripts - add termination of all processes upon interruption (ctrl+c)

--- nanoGPT/test_local_gen.sh
+++ nanoGPT/test_local_gen.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 
 # Test model on a single device and record times
-# Perform 10 runs with the same model - the "name" is the 1st command line arg
+# Perform a number of runs with the same model - the "name" is the 1st command
+# line arg and the number of iterations is the 2nd one
 # Example usage:
-#       ./test_local_gen.sh 7layers
+#       ./test_local_gen.sh 7layers 100
+
+# Clean shutdown (catch dangling processes - finisher and intermediate)
+trap 'kill $(pgrep -f 'sample.py')' INT
 
 if [ "$#" -eq 0 ];
 then
     echo "ERROR: missing model type/name in args"
-    exit
+    exit 1
 fi
-for i in $(seq 1 10);
+if [ ! -f "$(dirname "${0}")/data/shakespeare/out/ckpt_${1}.pt" ];
+then
+    echo "ERROR: $(dirname "${0}")/data/shakespeare/out/ckpt_${1}.pt does not exist"
+    exit 2
+fi
+
+n_iter=${2:-10}
+for i in $(seq 1 "${n_iter}");
 do
     echo "Launching run number ${i}"
-    python3 "$(dirname $0)"/sample.py \
-        --ckpt="$(dirname $0)"/data/shakespeare/out/ckpt_"$1".pt \
-        --time-run="$(dirname $0)"/logs/run_times_single_"$1".csv \
+    python3 "$(dirname "${0}")"/sample.py \
+        --ckpt="$(dirname "${0}")"/data/shakespeare/out/ckpt_"${1}".pt \
+        --time-run="$(dirname "${0}")"/logs/run_times_single_"${1}".csv \
         --verb
 done
--- nanoGPT/test_mdi_local.sh
+++ nanoGPT/test_mdi_local.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 # Test model on a single device and record times
-# Perform 10 runs with the same model - the "name" is the 1st command line arg
+# Perform a number of runs with the same model - the "name" is the 1st command
+# line arg and the number of iterations is the 2nd one
 # Example usage:
-#       ./test_local_gen.sh 7layers
+#       ./test_mdi_local.sh 7layers 100
+
+# Clean shutdown (catch dangling processes - finisher and intermediate)
+trap 'kill $(pgrep -f 'finisher.py'); kill $(pgrep -f 'intermediate.py'); kill $(pgrep -f 'starter.py')' INT
 
 if [ "$#" -eq 0 ];
 then
@@ -11,18 +15,19 @@ then
     exit 1
 fi
 
-if [ ! -f "$(dirname $0)"/data/shakespeare/out/ckpt_"$1".pt ];
+if [ ! -f "$(dirname "${0}")/data/shakespeare/out/ckpt_${1}.pt" ];
 then
-    echo "ERROR: $(dirname ${0})/data/shakespeare/out/ckpt_${1}.pt does not exist"
+    echo "ERROR: $(dirname "${0}")/data/shakespeare/out/ckpt_${1}.pt does not exist"
     exit 2
 fi
 
-for i in $(seq 1 10);
+n_iter=${2:-10}
+for i in $(seq 1 "${n_iter}");
 do
     echo "Launching run number ${i}"
-    python3 "$(dirname ${0})"/finisher.py &
-    python3 "$(dirname ${0})"/intermediate.py &
-    python3 "$(dirname ${0})"/starter.py \
-        --ckpt="$(dirname ${0})"/data/shakespeare/out/ckpt_"${1}".pt \
-        --time-run="$(dirname ${0})"/logs/run_times_mdi_single_"${1}".csv
+    python3 "$(dirname "${0}")"/finisher.py &
+    python3 "$(dirname "${0}")"/intermediate.py &
+    python3 "$(dirname "${0}")"/starter.py \
+        --ckpt="$(dirname "${0}")"/data/shakespeare/out/ckpt_"${1}".pt \
+        --time-run="$(dirname "${0}")"/logs/run_times_mdi_single_"${1}".csv
 done
