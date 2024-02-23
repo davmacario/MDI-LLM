2d422fd9fd6e167e9f7c00667f31ca1a0f544ddd
davmacario <dav.macario@gmail.com>
2024-02-23 11:39:49 -0600

    [feat]: add function to plot samples vs. time

--- nanoGPT/sub/utils.py
+++ nanoGPT/sub/utils.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
 import math
-from typing import Any, Dict, Mapping, Tuple, Union
+import os
+from typing import Any, Dict, List, Mapping, Tuple, Union
 
+import matplotlib.pyplot as plt
 import torch
 from numpy.typing import NDArray
 from torch import nn
@@ -346,3 +348,47 @@ def count_model_layers(model_params: Dict[str, Any]) -> int:
     ]
     layers_unique = list(set([".".join(k.split(".")[:3]) for k in layer_keys]))
     return len(layers_unique)
+
+
+# ---------- PLOTS -------------------------------------------------------------
+file_dir = os.path.dirname(__file__)
+
+
+def plot_tokens_per_time(
+    tok_time: List[Union[Tuple, List[Tuple]]],
+    out_path: str = os.path.join(file_dir, "..", "img", "tokens_time.png"),
+    disp: bool = True,
+):
+    """
+    Plot a graph representing the number of generated tokens in time.
+
+    Args:
+        tok_time: list of couples, where the 1st element is the number of
+            samples and the 2nd element is the time at which it was generated.
+            It can also be a list of list of couples (multiple samples); in this
+            case, the plot will distinguish between the different samples
+        out_path: path of the produced output image
+        disp: if true, the image will also be displayed at runtime
+    """
+    assert len(tok_time) >= 1
+
+    fig = plt.figure()
+    if isinstance(tok_time[0], Tuple):
+        time = [x[1] for x in tok_time]
+        n_samples = [x[0] for x in tok_time]
+        plt.plot(time, n_samples)
+        plt.title("Number of generated samples vs. time - MDI")
+    elif isinstance(tok_time[0], List):
+        for i, sublist in enumerate(tok_time):
+            time = [x[1] for x in sublist]
+            n_samples = [x[0] for x in sublist]
+            plt.plot(time, n_samples, label=f"Sample {i + 1}")
+            plt.legend()
+        plt.title("Number of generated samples vs. time - standalone")
+    plt.xlabel("Time (s)")
+    plt.ylabel("N. samples")
+    plt.grid()
+    plt.tight_layout()
+    fig.savefig(out_path)
+    if disp:
+        plt.show()
