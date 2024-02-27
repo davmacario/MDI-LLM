fa496157974e31a74d3ff1bca6df6a8377c6a360
davmacario <dav.macario@gmail.com>
2024-02-26 19:20:35 -0600

    [fix]: spelling

--- nanoGPT/sub/model_dist.py
+++ nanoGPT/sub/model_dist.py
@@ -1081,7 +1081,7 @@ class GPTDistributed:
         )
 
         global MODEL_TYPE
-        MODEL_TYPE = f"{self.n_layers_tot}layers_{self.model_ckpt['model_args']['block_size']}ckpt"
+        MODEL_TYPE = f"{self.n_layers_tot}layers_{self.model_ckpt['model_args']['block_size']}ctx"
 
         # Extract tokenizer metadata information and check it exists
         if (
