5dc573bed528be75d9d0ff859184da5498174aea
davmacario <dav.macario@gmail.com>
2024-03-04 10:57:35 -0600

    [feat]: improve style assignment for plots

--- nanoGPT/plot_results.py
+++ nanoGPT/plot_results.py
@@ -7,7 +7,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+from sub.utils import remove_prefix
+
 script_dir = os.path.dirname(__file__)
+line_styles = ["-", "--", "-.", ":"]
+
+"""
+Example usage:
+    python3 plot_results.py 12layers 3samples
+"""
 
 if __name__ == "__main__":
     # Parse arg: model type
@@ -27,11 +35,29 @@ if __name__ == "__main__":
     ), f"Error: folder not found {tok_t_folder}"
 
     fig = plt.figure(figsize=(12, 6))
+    ind_style_mdi = 0
+    ind_style_standalone = 0
     for fname in os.listdir(tok_t_folder):
         # Ugly :/
         if model_type in fname and samples_info in fname:
-            label = "MDI" if "mdi" in fname else "Standalone"
-            style = "b" if "mdi" in fname else "r"
+            if "mdi" in fname:
+                full_model_spec = remove_prefix(
+                    os.path.splitext(fname)[0], "tokens_time_samples_mdi_"
+                )
+                label = f"MDI {full_model_spec}"
+                style = "b" + line_styles[ind_style_mdi]
+                ind_style_mdi = (ind_style_mdi + 1) % len(line_styles)
+            else:
+                full_model_spec = remove_prefix(
+                    os.path.splitext(fname)[0],
+                    "tokens_time_samples_standalone_",
+                )
+                label = f"Standalone {full_model_spec}"
+                style = "r" + line_styles[ind_style_standalone]
+                ind_style_standalone = (ind_style_standalone + 1) % len(
+                    line_styles
+                )
+
             points = pd.read_csv(
                 os.path.join(tok_t_folder, fname),
                 sep=",",
