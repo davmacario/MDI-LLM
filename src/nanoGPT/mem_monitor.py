908b95497c1dc98a10f0f5d149f70c996ec4dba5
davmacario <dav.macario@gmail.com>
2024-03-05 15:00:07 -0600

    [fix]: repo structure

---
+++ src/nanoGPT/mem_monitor.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import argparse
+import subprocess
+import time
+
+import psutil
+
+
+def start_program():
+    """
+    Launch program you want to monitor the memory usage of.
+    """
+    pass
+
+
+def monitor_memory():
+    """
+    Store the measured memory usage on output file.
+    """
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
