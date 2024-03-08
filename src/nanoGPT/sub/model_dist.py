8e3c9c1750cf40a353293cd99d9d67aa29370218
davmacario <dav.macario@gmail.com>
2024-03-07 18:57:50 -0600

    [fix]: change method do measure object size

--- src/nanoGPT/sub/model_dist.py
+++ src/nanoGPT/sub/model_dist.py
@@ -1228,7 +1228,7 @@ class GPTDistributed:
         n_ret = 0
         if VERB:
             print(f"Sending {req_type} request to {addr}")
-            print(f"Payload: {get_obj_size(content)} Bytes")
+            print(f"Payload: {len(pickle.dumps(content))} Bytes")
         try:
             # Specify timeout
             ret = req_func(
