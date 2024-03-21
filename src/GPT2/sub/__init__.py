0884391782e7d61d19e535a639aabb0607628189
davmacario <dav.macario@gmail.com>
2024-03-21 17:52:44 -0500

    [fix]: cleanup deleted files

--- src/GPT2/sub/__init__.py
+++ src/GPT2/sub/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from . import config, data_loader, model, model_dist, parser, server_config
+from . import config, data_loader, model, model_dist, parser
 from .bpe_tokenizer import BPETokenizer
 from .char_tokenizer import CharacterTokenizer
 
@@ -11,6 +11,5 @@ __all__ = [
     "model",
     "model_dist",
     "parser",
-    "server_config",
     "BPETokenizer",
 ]
