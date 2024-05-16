0d49b914695fe9bc49b65d696d3da413d1f4794f
davmacario <dav.macario@gmail.com>
2024-05-16 12:02:41 -0500

    feat: implement start function and others

--- src/llama/sub/model_dist.py
+++ src/llama/sub/model_dist.py
@@ -37,7 +37,7 @@ import requests
 import tiktoken
 import torch
 import torch.nn as nn
-from sub.config import DTYPE, HEADERLENGTH, TEMPERATURE, TOP_K
+from sub.config import DTYPE, HEADERLENGTH, N_LAYERS_NODES, TEMPERATURE, TOP_K
 from sub.gptserver import GPTServer
 from sub.model import GPT, Block, Config, build_mask_cache, build_rope_cache
 from sub.submodels import SecondaryNode, StarterNode
@@ -190,9 +190,10 @@ class GPTDistributed:
                 chunk_path=self.chunk_path,
                 tokenizer_dir=self.ckpt_dir,
                 model_device=self.torch_device,
+                **kwargs,
             )
         elif "secondary" in self.node_type:
-            # FIXME: secondary node may be completely agnostic of the used model and 
+            # FIXME: secondary node may be completely agnostic of the used model and
             # receive the model config (and the chunk) at initialization
             assert (
                 self.ckpt_dir or self.chunk_path
@@ -200,7 +201,9 @@ class GPTDistributed:
 
             # Can either pass "secondary:ind" or secondary_index=ind
             split_type = self.node_type.split(":")
-            self.secondary_index = secondary_index if len(split_type) < 2 else int(split_type[1])
+            self.secondary_index = (
+                secondary_index if len(split_type) < 2 else int(split_type[1])
+            )
             assert self.secondary_index is not None
 
             self.node_type = f"secondary:{self.secondary_index}"
@@ -243,4 +246,219 @@ class GPTDistributed:
                 model_config=self.model_config,
                 chunk_path=self.chunk_path,
                 model_device=self.torch_device,
+                **kwargs,
             )
+
+    def start(
+        self,
+        *,
+        n_samples: Optional[int] = None,
+        tokens_per_sample: Optional[int] = None,
+        prompt: Optional[str] = None,
+    ):
+        """
+        Main class entrypoint.
+        Start the application; for the starter node, this triggers the initialization of
+        other nodes and launches generation.
+        For secondary nodes, this starts an infinite loop where the node will wait to be
+        initialized and perform inference.
+
+        Args:
+            *,
+            n_samples (starter only): number of samples to be generated; NOTE: if the
+                number is lower than the number of nodes, the generation will not
+                benefit from pipelining.
+            tokens_per_sample (starter only): number of samples to be *generated*
+                (regardless of the prompt length).
+            prompt (starter only): prompt (as received from command line) - can be
+                FILE:<...>
+        """
+        if self.node_type == "starter":
+            assert n_samples and tokens_per_sample
+            # Init. nodes, launch iterations
+            if not self.configure_nodes(n_samples=n_samples):
+                raise RuntimeError("Unable to initialize network nodes!")
+
+            try:
+                out_text, time_gen = self.gpt_serv.launch_starter(
+                    n_samples, tokens_per_sample, prompt
+                )
+                print("-------------------------------------------------")
+                print("Produced output:\n")
+                for i, smpl in enumerate(out_text):
+                    print("-------------------------------------------------")
+                    print(f"Sample {i + 1}:")
+                    print(smpl, "\n")  # [: find_eot(smpl)], "\n")
+                print("-------------------------------------------------")
+                print(f"Total generation time: {time_gen}")
+            except KeyboardInterrupt:
+                # TODO: shutdown
+                print("Node was stopped!")
+        else:
+            try:
+                cp.engine.block()  # Same as while True: time.sleep(...)
+            except KeyboardInterrupt:
+                # TODO: Shutdown
+                print("Node was stopped!")
+
+    # ---------------------------------------------------------------------------------
+
+    def configure_nodes(self, n_samples: int) -> int:
+        """
+        Send POST requests to the other nodes to inform them of their role, the number
+        of samples, and including their chunk of model.
+
+        Information sent:
+            - Node role ("role")
+            - Model config (GPTConfig as dict) ("model_config")
+            - Model parameters ("params") - from pickle.dumps() - if not split before
+            - Previous node information - from json file ("prev_node")
+            - Next node information - from json ("next_node")
+
+        Returns:
+            1 if success
+            0 if at least 1 node fails
+        """
+        assert self.n_nodes is not None
+        assert self.chunk_path is not None
+        node_chunks_dir = self.chunk_path.parent
+        if self.node_type != "starter":
+            raise ValueError("This method can only be called on starter nodes!")
+        if not self.model_config:
+            raise ValueError("The model configuration was not loaded!")
+
+        out = 1  # Return code
+        # Store the prev and next in a smart way
+        prev = self.node_config["nodes"]["starter"]
+        if self.n_secondary == 1:
+            next = self.node_config["nodes"]["starter"]
+        elif self.n_secondary > 1:
+            next = self.node_config["nodes"]["secondary"][1]
+        else:
+            warnings.warn("No secondary nodes found! Running standalone")
+            return out
+
+        # Secondary nodes config
+        for i, sec_node in enumerate(self.node_config["nodes"]["secondary"]):
+            if VERB:
+                print(f"Initializing secondary node n.{i}")
+
+            curr_msg = self.init_msg.copy()  # FIXME: maybe put before loop
+            curr_msg["role"] = f"secondary:{i}"
+            curr_msg["model_config"] = self.model_config.asdict()
+            curr_msg["n_nodes"] = self.n_nodes
+            curr_msg["n_local_layers"] = N_LAYERS_NODES[self.n_nodes][
+                self.model_config.n_layer
+            ]["N_LAYERS_SECONDARY"]
+            curr_msg["n_samples"] = n_samples
+            curr_msg["prev_node"] = prev
+            curr_msg["next_node"] = next
+
+            if not self.model_was_split:
+                # TODO: load from file; store in local var to automatically clear mem
+                chunk_path = node_chunks_dir / f"model_secondary{i}.pth"
+                curr_msg["params"] = torch.load(chunk_path, device="cpu")
+
+            # Update next and prev for next iteration
+            prev = sec_node
+            if i == self.n_secondary - 1:  # Last iter in loop - finished
+                next = None
+            elif i == self.n_secondary - 2:  # Second to last iter
+                next = self.node_config["nodes"]["starter"]
+            else:
+                next = self.node_config["nodes"]["secondary"][i + 2]
+
+            # Send POST request
+            target_addr = sec_node["addr"]
+            target_port = sec_node["communication"]["port"]
+
+            addr = f"http://{target_addr}:{target_port}/init"
+            out *= self._request_to_node("post", addr, curr_msg)
+
+            if not out:
+                if VERB:
+                    print("> Failed!")
+                logger_wp.error(f"Failed to initialize secondary node {i}!")
+                return out
+
+            if VERB:
+                print("> Success!")
+            logger_wp.info(f"Secondary node {i} was initialized successfully")
+
+        return out
+
+    def _request_to_node(
+        self, req_type: str, addr: str, content: Any, max_n_requests: int = 100
+    ) -> int:
+        """
+        Send an HTTP request containing a json-formatted string to a specified
+        target node.
+
+        Args:
+            req_type: type of HTTP request, can be "post" or "put"
+            addr: full address (http(s)://<ip>:<port>) of the target node
+            content: python dict containing the information
+            max_n_requests: maximum number of requests before failure
+
+        Returns:
+            1 if successful
+            0 if failed
+        """
+        if req_type.lower() == "post":
+            req_func = requests.post
+        elif req_type.lower() == "put":
+            req_func = requests.put
+        else:
+            raise ValueError(f"Unsupported request type '{req_type}'")
+        ret = None
+        n_ret = 0
+        if VERB:
+            print(f"Sending {req_type} request to {addr}")
+            print(f"Payload: {len(pickle.dumps(content))} Bytes")
+        try:
+            # Specify timeout
+            ret = req_func(
+                addr,
+                data=pickle.dumps(content),
+                timeout=100,
+            )
+
+            if ret.status_code == 413:
+                raise ConnectionError(f"Max payload for {req_type} was exceeded!")
+            logger_wp.debug(
+                f"Successful {req_type} request sent to {addr} - code {ret.status_code}"
+            )
+        except requests.exceptions.Timeout:
+            if VERB:
+                print("Connection timed out!")
+            logger_wp.warning(f"Request timed out!")
+            n_ret += 1
+        except:
+            logger_wp.warning(f"Unable to submit {req_type} request sent to {addr}")
+            n_ret += 1
+        while (ret is None or ret.status_code != 200) and n_ret < max_n_requests:
+            if VERB:
+                print(
+                    f"Unable to reach node ({addr}) - retrying in 2s ({n_ret}/{max_n_requests})"
+                )
+            time.sleep(2)
+            try:
+                ret = req_func(
+                    addr,
+                    data=pickle.dumps(content),
+                    timeout=10000,
+                )
+                logger_wp.debug(
+                    f"Successful {req_type} request sent to {addr} - code {ret.status_code}"
+                )
+            except requests.exceptions.Timeout:
+                if VERB:
+                    print("Connection timed out!")
+                logger_wp.warning(f"Request timed out!")
+            except:
+                logger_wp.warning(f"Unable to submit {req_type} request sent to {addr}")
+            n_ret += 1
+
+        if ret is not None and ret.status_code == 200:
+            return 1
+        return 0
