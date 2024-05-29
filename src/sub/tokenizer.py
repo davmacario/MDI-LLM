9f7d327b4fc5e6a49f9ebae667e591a396689197
davmacario <dav.macario@gmail.com>
2024-05-28 19:39:59 -0500

    fix: make tensor 1D before decoding

--- src/sub/tokenizer.py
+++ src/sub/tokenizer.py
@@ -144,5 +144,6 @@ class Tokenizer:
         return torch.tensor(tokens, dtype=torch.int, device=device)
 
     def decode(self, tensor: torch.Tensor) -> str:
+        tensor = tensor.view(-1, 1)
         tokens = [tensor.item()] if tensor.ndim == 0 else tensor.tolist()
-        return self.processor.decode(tokens)[0]  # FIXME: why multiple items?
+        return self.processor.decode(tokens)
