e3948a4c89aa485ee17da9e9d561c6fda0a1d658
davmacario <dav.macario@gmail.com>
2024-06-07 11:54:54 -0500

    refactor: export connections module

--- src/sub/__init__.py
+++ src/sub/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from . import config, model, prompts, tokenizer, utils, submodels, gptserver, typing
+from . import config, model, prompts, tokenizer, utils, submodels, gptserver, typing, connections
 from .model import GPT, Config
 from .prompts import PromptStyle, get_user_prompt
 from .tokenizer import Tokenizer
@@ -23,4 +23,5 @@ __all__ = [
     "typing",
     "GPTDistributed",
     "functional",
+    "connections",
 ]
