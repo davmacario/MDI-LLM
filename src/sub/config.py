1c199663e5d8b7af8f9644c8c6f61a9e786c0cb7
davmacario <dav.macario@gmail.com>
2024-06-12 01:39:29 -0500

    fix: mistral 7b v0.3 config

--- src/sub/config.py
+++ src/sub/config.py
@@ -1495,21 +1495,6 @@ mistral = [
         mlp_class_name="LLaMAMLP",
         intermediate_size=14336,
     ),
-    dict(
-        name="Mistral-7B-{}v0.3",
-        hf_config=dict(org="mistralai", name="Mistral-7B-{}v0.3"),
-        padded_vocab_size=32000,
-        block_size=32768,
-        n_layer=32,
-        n_query_groups=8,
-        rotary_percentage=1.0,
-        parallel_residual=False,
-        bias=False,
-        norm_class_name="RMSNorm",
-        norm_eps=1e-05,
-        mlp_class_name="LLaMAMLP",
-        intermediate_size=14336,
-    ),
     # https://huggingface.co/mistralai/Mixtral-8x7B-v0.1/blob/main/config.json
     dict(
         name="Mixtral-8x7B-{}v0.1",
@@ -1573,6 +1558,43 @@ configs.append(
         intermediate_size=14336,
     )
 )
+configs.append(
+    # https://huggingface.co/mistralai/Mistral-7B-v0.3/blob/main/config.json
+    dict(
+        name="Mistral-7B-v0.3",
+        hf_config=dict(org="mistralai", name="Mistral-7B-v0.3"),
+        padded_vocab_size=32768,
+        block_size=32768,
+        n_layer=32,
+        n_query_groups=8,
+        rotary_percentage=1.0,
+        parallel_residual=False,
+        bias=False,
+        norm_class_name="RMSNorm",
+        norm_eps=1e-05,
+        mlp_class_name="LLaMAMLP",
+        intermediate_size=14336,
+    )
+)
+configs.append(
+    # https://huggingface.co/mistralai/Mistral-7B-Instruct-v0.3/blob/main/config.json
+    dict(
+        name="Mistral-7B-Instruct-v0.3",
+        hf_config=dict(org="mistralai", name="Mistral-7B-Instruct-v0.3"),
+        padded_vocab_size=32768,
+        block_size=32768,
+        n_layer=32,
+        n_query_groups=8,
+        rotary_percentage=1.0,
+        parallel_residual=False,
+        bias=False,
+        norm_class_name="RMSNorm",
+        norm_eps=1e-05,
+        mlp_class_name="LLaMAMLP",
+        intermediate_size=14336,
+    )
+)
+
 
 
 ############
