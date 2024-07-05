aa1e306315f4592228ec60503c337c0d0880902d
davmacario <dav.macario@gmail.com>
2024-07-05 15:53:56 -0500

    fix: prevent issues when installing packages

--- scripts/jetson-setup.sh
+++ scripts/jetson-setup.sh
@@ -5,8 +5,10 @@ sudo apt update
 sudo apt upgrade -y
 
 echo "Installing requirements"
-sudo apt install git vim htop neofetch curl wget tmux -y
-sudo apt install python3.8 python3.8-venv python3-pip -y
+PACKAGES="git vim htop neofetch curl wget tmux nvidia-jetpack python3.8 python3.8-venv python3-pip"
+for p in $PACKAGES; do
+    sudo apt install "$p" -y
+done
 python3.8 -m pip install --upgrade pip
 sudo pip3 install -U jetson-stats
 
