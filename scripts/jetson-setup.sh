c96070f80600120550f32ea29878230703a39b1b
davmacario <dav.macario@gmail.com>
2024-07-05 13:51:18 -0500

    feat: add setup scripts for Jetsons

---
+++ scripts/jetson-setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Updating system"
+sudo apt update
+sudo apt upgrade -y
+
+echo "Installing requirements"
+sudo apt install git vim htop neofetch curl wget tmux -y
+sudo apt install python3.8 python3.8-venv python3-pip -y
+python3.8 -m pip install --upgrade pip
+sudo pip3 install -U jetson-stats
+
