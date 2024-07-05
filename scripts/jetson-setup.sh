e73d1ac7403dca9e6d22bd1546327b424627c1c2
davmacario <dav.macario@gmail.com>
2024-07-05 15:40:01 -0500

    fix: add tailscale install command

--- scripts/jetson-setup.sh
+++ scripts/jetson-setup.sh
@@ -10,3 +10,5 @@ sudo apt install python3.8 python3.8-venv python3-pip -y
 python3.8 -m pip install --upgrade pip
 sudo pip3 install -U jetson-stats
 
+echo "Installing tailscale (needs to be set up afterwards)"
+curl -fsSL https://tailscale.com/install.sh | sh
